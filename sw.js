d0a0f1e9d2ecf7891359246bf5ba119f936a3175
gregtyler <gregtyler@users.noreply.github.com>
2022-08-30 10:21:27 +0000

    deploy: debc224ef8ac3357624e05cbd2005634f84ad986

--- sw.js
+++ sw.js
@@ -1,2 +1,41 @@
-(()=>{self.addEventListener("install",function(e){e.waitUntil(caches.open("scoreboard").then(function(n){return n.addAll(["/","/index.html","/index.js","/images/favicon.png","/images/favicon-144.png","/images/favicon-192.png","/images/favicon-512.png"])}))});self.addEventListener("fetch",function(e){e.respondWith(async function(){return await caches.match(e.request)||fetch(e.request)}())});self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(n){return Promise.all(n.map(function(t){return caches.delete(t)}))}))});})();
-//# sourceMappingURL=sw.js.map
+"use strict";
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open("scoreboard").then(function (cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/index.js",
+        "/index.css",
+        "/images/favicon.png",
+        "/images/favicon-144.png",
+        "/images/favicon-192.png",
+        "/images/favicon-512.png",
+        "/fonts/MaterialSymbolsOutlined[FILL,GRAD,opsz,wght].ttf",
+        "/fonts/RobotoFlex-VariableFont_GRAD,XTRA,YOPQ,YTAS,YTDE,YTFI,YTLC,YTUC,opsz,slnt,wdth,wght.ttf",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    (async function () {
+      const response = await caches.match(event.request);
+      return response || fetch(event.request);
+    })()
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
